ce93eefb9914868354563045285a323ac9999af8
Zayne Lee(Seongdo Lee) <63707025+ZayneLee@users.noreply.github.com>
2024-12-15 08:13:47 +0900

    Create news.py

---
+++ news.py
@@ -0,0 +1,14 @@
+import requests as req
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+
+res = req.get("https://search.naver.com/search.naver?where=news&query=%EC%86%8D%EB%B3%B4&sm=tab_tmr&nso=so:r,p:all,a:all&sort=0")
+soup = bs(res.text, "lxml")
+title = soup.select("a.news_tit")
+titleList = []
+for i in title:
+    titleList.append(i.text)
+
+dic = {"뉴스제목" : titleList}
+df = pd.DataFrame(dic)
+df.to_csv("data1.csv", encoding="utf-8", index=False)
